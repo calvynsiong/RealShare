5dba2c33b7babf08a37b6aba1162badc62357df1
Calvyn <calvyn360@gmail.com>
2021-08-27 20:39:40 -0400

    Initializing tailwind + ts

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -3,39 +3,12 @@ import Link from 'next/link';
 import Head from 'next/head';
 
 type Props = {
-  children?: ReactNode;
   title?: string;
+  children?: ReactNode;
 };
 
 const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet='utf-8' />
-      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-    </Head>
-    <header>
-      <nav>
-        <Link href='/' className='p-4'>
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href='/about'>
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href='/users'>
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href='/api/users'>Users API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
+  <div className='p-4'>Hello World</div>
 );
 
 export default Layout;
