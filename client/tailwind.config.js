9c7ae3d4597dc6b463595cfe17820ff8731a00b7
Calvyn <calvyn360@gmail.com>
2021-09-02 19:30:51 -0400

    Changed config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,11 +4,7 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './layout/**/*.{js,ts,jsx,tsx}',
-  ],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './layout/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {},
