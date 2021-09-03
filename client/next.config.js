8e25ebb83158a95c94de50f6006cbbd202000df9
Calvyn <calvyn360@gmail.com>
2021-09-02 20:25:20 -0400

    Update next config

--- client/next.config.js
+++ client/next.config.js
@@ -2,4 +2,45 @@ module.exports = {
   // Webpack 5 is enabled by default
   // You can still use webpack 4 while upgrading to the latest version of Next.js by adding the "webpack5: false" flag
   webpack5: false,
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: require.resolve('babel-loader'),
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.png|svg|jpg|gif$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|svg)$/,
+        exclude: /node_modules/,
+        loader: 'file-loader',
+        options: {
+          limit: 1024,
+          name: '[name].[ext]',
+          publicPath: './public/images/',
+        },
+      },
+    ],
+  },
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.mdx/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: '@mdx-js/loader',
+          options: pluginOptions.options,
+        },
+      ],
+    });
+
+    return config;
+  },
 };
