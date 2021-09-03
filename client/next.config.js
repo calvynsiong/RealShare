6ae3951fd05229122502ba0a8a23552bf9389a79
Calvyn <calvyn360@gmail.com>
2021-09-02 20:33:04 -0400

    update

--- client/next.config.js
+++ client/next.config.js
@@ -20,18 +20,4 @@ module.exports = {
       },
     ],
   },
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.mdx/,
-      use: [
-        options.defaultLoaders.babel,
-        {
-          loader: '@mdx-js/loader',
-          options: pluginOptions.options,
-        },
-      ],
-    });
-
-    return config;
-  },
 };
